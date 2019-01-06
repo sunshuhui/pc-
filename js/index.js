0a4e11559dba6263ae854ca27021defcb5fafd2b
ssh <sunshuhui@163.com>
2019-01-07 00:18:34 +0800

    完成了内容区域2和3

--- js/index.js
+++ js/index.js
@@ -15,6 +15,14 @@ window.addEventListener('DOMContentLoaded', function () {
     //处理头部js代码
     headerHandle();
     function headerHandle() {
+        /*var img = new Image();
+        img.src = './imgs/home.png';
+        img.onload = function () {
+          //初始化时小箭头来到第一个li下面
+          arrowNode.style.left = headerLisNodes[0].getBoundingClientRect().left + headerLisNodes[0].offsetWidth / 2
+            - arrowHalfWidth + 'px';
+          headerDownNodes[0].style.width = '100%';
+        }*/
 
         //初始化时小箭头来到第一个li下面
         arrowNode.style.left = headerLisNodes[0].getBoundingClientRect().left + headerLisNodes[0].offsetWidth / 2
@@ -45,7 +53,7 @@ window.addEventListener('DOMContentLoaded', function () {
         //让内容区ul运动
         contentUlNode.style.top = - nowIndex * contentHeight + 'px';
     }
-
+    move(2)
     //内容区js代码
     contentHandle();
     function contentHandle() {
@@ -90,7 +98,7 @@ window.addEventListener('DOMContentLoaded', function () {
                         break;
                 }
 
-            }, 100);
+            }, 200);
             //禁止默认行为
             event.preventDefault && event.preventDefault();
             return false;
@@ -104,5 +112,82 @@ window.addEventListener('DOMContentLoaded', function () {
             - arrowHalfWidth + 'px';
         contentUlNode.style.top = - nowIndex * contentHeight + 'px';
     }
+
+    //第一屏js代码
+    firstViewHandle();
+    function firstViewHandle() {
+        var homeCarouselNodes = document.querySelectorAll('.home-carousel li');
+        var homePointNodes = document.querySelectorAll('.home-point li');
+        var homeNode = document.querySelector('.home');
+
+        var lastIndex = 0;
+        var nowIndex = 0;
+        var lastTime = 0;
+        var timer = null;
+
+        for (var i = 0; i < homePointNodes.length; i++) {
+            homePointNodes[i].index = i;
+            homePointNodes[i].onclick = function () {
+                //函数节流：规定时间内，只让第一次操作生效，后面不生效
+                //如果点击的时间间隔小于2秒，不生效
+                var nowTime = Date.now();
+                console.log(nowTime);  //得到当前的格林时间 单位ms
+                if (nowTime - lastTime <= 2000) return;
+                //同步上一次点击时间
+                lastTime = nowTime;
+
+                //同步nowIndex的值
+                nowIndex = this.index;
+                //如果点击同一个就啥也不做
+                if (nowIndex === lastIndex) return;
+
+                if (nowIndex > lastIndex) {
+                    //点击是右边  右边加上right-show  左边加上left-hide
+                    homeCarouselNodes[nowIndex].className = 'common-title right-show';
+                    homeCarouselNodes[lastIndex].className = 'common-title left-hide';
+                } else {
+                    //点击是左边
+                    homeCarouselNodes[nowIndex].className = 'common-title left-show';
+                    homeCarouselNodes[lastIndex].className = 'common-title right-hide';
+                }
+                //修改小圆点的显示
+                homePointNodes[lastIndex].className = '';
+                this.className = 'active';
+
+                //同步上一次的值
+                lastIndex = nowIndex;
+
+            }
+        }
+
+
+        homeNode.onmouseenter = function () {
+            clearInterval(timer);
+        }
+
+        homeNode.onmouseleave = autoPlay;
+
+        //自动轮播
+        autoPlay();
+        function autoPlay() {
+            timer = setInterval(function () {
+                nowIndex++;
+
+                if (nowIndex >= 4) nowIndex = 0;
+
+                homeCarouselNodes[nowIndex].className = 'common-title right-show';
+                homeCarouselNodes[lastIndex].className = 'common-title left-hide';
+
+                homePointNodes[lastIndex].className = '';
+                homePointNodes[nowIndex].className = 'active';
+
+                lastIndex = nowIndex;
+            }, 3500)
+        }
+
+    }
+
+
+    //第二屏js代码
 
 })
